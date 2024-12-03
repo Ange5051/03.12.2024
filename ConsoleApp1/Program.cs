3ed9731aacbcdeac850c0ebbcd9c29c0ce7f17e4
Ange5051 <147865401+Ange5051@users.noreply.github.com>
2024-12-03 14:22:18 +0200

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    /// <summary>
+    /// Заполнение одномерного массива с клавиатуры и вывод на экран
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = GetSize();
+            int[] array = Input(size);
+            Output(array);
+        }
+        static int GetSize()
+        {
+            Console.Write("Введите одномерный массив: ");
+            int size;
+            while (!int.TryParse(Console.ReadLine(), out size) || size <= 0)
+            {
+                Console.WriteLine("Ошибка");
+            }
+            return size;
+        }
+        static int[] Input(int size)
+        {
+            int[] array = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write($"array[{i}]");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            return array;
+        }
+        static void Output(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"array[{i}]={array[i]}");
+            }
+        }
+    }
+
+}
